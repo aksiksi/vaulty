dc194dc9c77af3d60da6e81dbef1e6b48694be05
Assil Ksiksi <cyph0nik@gmail.com>
2019-12-30 17:35:57 -0500

    Added basic Postfix configs

---
+++ massu-mail/setup/provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get install postfix
+
+# Setup the virtual mail user
+sudo mkdir /var/vmail
+sudo groupadd -g 5000 vmail
+sudo useradd -m -d /var/vmail -s /bin/false -u 5000 -g vmail vmail
+sudo chown -R vmail:vmail /var/vmail
+
+# Copy over Postfix configs
+cp $SETUP_DIR/postfix/*  /etc/postfix/
+
+# FIXME: Make this setup Postgres instead (?)
+/usr/sbin/postmap /etc/postfix/vmail
