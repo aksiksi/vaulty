7dcf5f8fded85acab6e295e5e8e259b3197262a2
Assil Ksiksi <cyph0nik@gmail.com>
2020-05-25 18:51:44 -0400

    Web: Add basic Django env loading

--- vaulty-web/vaulty/vaulty/wsgi.py
+++ vaulty-web/vaulty/vaulty/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import dotenv
+
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vaulty.settings')
 
 application = get_wsgi_application()
